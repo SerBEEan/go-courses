8c08d948e86d790f47ee11984c93209591afa4a6
SerBEEan <sergey.atwork7@gmail.com>
2022-11-17 21:45:01 +0300

    сделал конвертер

--- lection_2/hw/converter/converter/1.go
+++ lection_2/hw/converter/converter/1.go
@@ -1,2 +1,36 @@
 package converter
 
+type converter struct {
+	symbols []string
+	values  []int
+}
+
+type Converter interface {
+	IntToRoman(num int) string
+}
+
+func (c *converter) IntToRoman(num int) string {
+	number := num
+	result := ""
+
+	for i := 0; i < len(c.values); i++ {
+		currentValue := c.values[i]
+		currentSymbol := c.symbols[i]
+
+		for number >= currentValue {
+			result += currentSymbol
+			number -= currentValue
+		}
+	}
+
+	return result
+}
+
+func NewConverter(values []int, symbols []string) Converter {
+	converter := &converter{
+		values:  values,
+		symbols: symbols,
+	}
+
+	return converter
+}
